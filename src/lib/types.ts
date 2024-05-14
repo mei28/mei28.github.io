e7e409e1c32d21843ce3d5c1f0960d2bfad51cd0
mei <mei28aquarius@gmail.com>
2024-05-13 22:22:41 -1000

    sveltekit

---
+++ src/lib/types.ts
@@ -0,0 +1,77 @@
+import type { Color } from './utils/colors'
+
+export enum Platform {
+  GitHub = 'github',
+  Twitter = 'twitter',
+  Email = 'email',
+}
+
+export type Icon = `i-${string}-${string}`;
+
+export enum ContractType {
+  FullTime = 'Full-time',
+  PartTime = 'Part-time',
+  SelfEmployed = 'Self-employed',
+  Freelance = 'Freelance',
+  Contract = 'Contract',
+  Internship = 'Internship'
+}
+
+export type Asset = string | { light: string, dark: string };
+
+export interface Item<S extends string = string> {
+  slug: S;
+  name: string;
+  logo: Asset;
+  shortDescription: string;
+  description: string;
+  screenshots?: Array<{ src: string; label: string }>;
+}
+
+export interface Link {
+  to: string;
+  label: string;
+  newTab?: boolean;
+}
+
+export interface IconLink extends Link {
+  icon: Asset;
+}
+
+export interface SkillCategory<S extends string = string> {
+  slug: S;
+  name: string;
+}
+
+export interface Skill<S extends string = string> extends Omit<Item<S>, 'shortDescription'> {
+  color: string;
+  category?: SkillCategory;
+}
+
+export interface Project<S extends string = string> extends Item<S> {
+  links: Array<Link>;
+  color: Color;
+  period: {
+    from: Date;
+    to?: Date;
+  };
+  type: string;
+  skills: Array<Skill<S>>;
+}
+
+export interface Experience<S extends string = string> extends Project<S> {
+  company: string;
+  location: string;
+  contract: ContractType;
+}
+
+export interface Education<S extends string = string> extends Item<S> {
+  organization: string;
+  location: string;
+  period: {
+    from: Date;
+    to?: Date;
+  };
+  subjects: Array<string>;
+  degree: string;
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { useTitle } from "$lib/utils/helpers";
+  import MainTitle from "$lib/components/MainTitle/MainTitle.svelte";
+</script>
+
+<svelte:head>
+  <title>{useTitle(title, titleSuffix)}</title>
+</svelte:head>
+
+<div
+  class="col self-center flex-1 md:flex-row md:slef-stretch justify-center lg:justify-between items-center p-y-0px p-x-10px"
+>
+  <div class="md:flex-1 gap-10px">
+    <MainTitle classes="md:text-left">{name} {lastName},</MainTitle>
+    <p
+      class="text-[var(--tertiary-text)] text-center md:text-left text-[1.2em] font-extralight"
+    >
+      {description}
+    </p>
+    <div class="row justify-center md:justify-start p-y-15px p-x-0px gap-2">
+      {#each links as link}
+        <a
+          class="decoration-none"
+          href={`${isEmail(link.link) ? "mailto:" : ""}${link.link}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Icon
+            icon={getPlatfromIcon(link.platform)}
+            color={"var(--accent-text)"}
+            size={"20px"}
+          />
+        </a>
+      {/each}
+    </div>
+  </div>
+  <Carrousel items={skills ?? skillsItems} />
+</div>
