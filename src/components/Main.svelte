5f2ec37e68e6648ff411165b854d586a07422286
mei <mei28aquarius@gmail.com>
2024-05-17 13:24:36 -1000

    aupdate

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,4 +1,25 @@
 <script lang="ts">
+  import Step from "./Step.svelte";
+  let steps = [
+    {
+      name: "Smoljames Store",
+      icon: "fa-solid fa-cart-shopping",
+      description:
+        "Smoljames Store is a a merchanising store created with Next.js, Commerce.js & Stripe! Commerce.js is a CMS for managing project and Stripe is used for all transaction handling.",
+    },
+    {
+      name: "Utlimate Todos",
+      icon: "fa-solid fa-list-check",
+      description:
+        "Test for model robustness with customized test profiles and receive exhaustive reporting on the performance and potential vulnerabilities of your model.",
+    },
+    {
+      name: "Pokédex",
+      icon: "fa-solid fa-diagram-project",
+      description:
+        "Augment your datasets with our filters and dataset manipulations to ensure your models are trained on the highest quality datasets (coming soon).",
+    },
+  ];
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -22,17 +43,40 @@
         target="_blank"
       >
         <div
-          class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+          class="absolute py-10 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
         />
         <h4 class="relative z-9">Get in touch &rarr;</h4>
       </a>
     </div>
-    <div class="relative shadow-2xl grid place-items-center top-10">
+    <div class="relative shadow-2xl grid place-items-center">
       <img
         src={"images/profile.png"}
         alt="Profile image"
         class="object-fit:cover z-[2] max-h-[70vh]"
       />
+    </div>
+  </section>
+
+  <section id="projects" class="py-20 lg:py-32 flex flex-cal gpt-24">
+    <div class="flex flex-col gap-2 text-center">
+      <h6 class="text-lg sm:text-xl md:text-2xl">Projects</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        Curious to <span class="bizudp text-violet-400">see</span> my works?
+      </h3>
+    </div>
+    <a
+      href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      target="_blank"
+      class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+    >
+      <i class="fa-regular fa-circle-play" />
+      <p>Watch the video</p>
+    </a>
+
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+      <Step step={steps[0]}></Step>
+      <Step step={steps[1]}></Step>
+      <Step step={steps[2]}></Step>
     </div>
   </section>
 </main>
---
+++ src/components/Step.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let step;
+</script>
+
+<a href= {step.href} target="_blank" class= 'p-4 sm:p-5 md:p-8 flex flex-col gap-4 rounded-lg'>
+
+</a>
+  
