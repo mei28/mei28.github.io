de45ffd1ebda3eb4593353d7ca851530e04b3e3e
mei <mei28aquarius@gmail.com>
2024-07-27 15:08:31 +0900

    add

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
-import { Sections, sectionTitles, getNextSection } from '../sections';
+import { Sections, sectionTitles } from '../sections';
 import { projectsData } from '../data/projectsData';
 import '../styles/common.css';
 
@@ -11,7 +11,7 @@ interface Link {
 }
 
 interface Project {
-  imgSrc: string;
+  imgSrc: string | null;
   imgAlt: string;
   title: string;
   description: string;
@@ -19,10 +19,24 @@ interface Project {
 }
 
 const ProjectItem: React.FC<Project> = ({ imgSrc, imgAlt, title, description, links }) => {
+  const isExternalLink = (url: string) => {
+    return url.startsWith('http://') || url.startsWith('https://');
+  };
+
+  const renderImage = (src: string | null, alt: string) => {
+    if (!src) {
+      return null;
+    }
+
+    const imageUrl = isExternalLink(src) ? src : process.env.PUBLIC_URL + src;
+
+    return <img src={imageUrl} alt={alt} className="rounded-2xl w-9/10 h-auto" />;
+  };
+
   return (
     <div className="border border-gray-400 bg-gray-50 p-4 rounded-xl mb-6">
       <div className="mb-4">
-        <img src={imgSrc} alt={imgAlt} className="rounded-2xl w-9/10 h-auto" />
+        {renderImage(imgSrc, imgAlt)}
       </div>
       <h2 className="text-xl font-semibold mb-2 text-black">{title}</h2>
       <p className="text-gray-600 mb-4">{description}</p>
@@ -30,7 +44,7 @@ const ProjectItem: React.FC<Project> = ({ imgSrc, imgAlt, title, description, li
         {links.map((link, index) => (
           <button
             key={index}
-            className="px-4 py-2 border border-gray-400 rounded-full transition hover:bg-gray-200"
+            className="px-4 py-2 bg-gray-200 rounded-full hover:bg-gray-300 transition"
             onClick={() => window.location.href = link.url}
           >
             {link.label}
@@ -47,7 +61,7 @@ const Project: React.FC = () => {
       <p className="text-center text-xl mb-4">Browse My Recent</p>
       <h1 className="text-center text-3xl font-bold mb-12">{sectionTitles[Sections.Projects]}</h1>
 
-      <div className="flex flex-col gap-6">
+      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-2">
         {projectsData.map((project, index) => (
           <ProjectItem
             key={index}
@@ -58,6 +72,12 @@ const Project: React.FC = () => {
             links={project.links}
           />
         ))}
+      </div>
+
+      <div className="text-center mt-8">
+        <a href="/#/all-projects" className="inline-block px-6 py-3 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700">
+          View My All Projects! <FontAwesomeIcon icon={faAnglesRight} className="ml-2" />
+        </a>
       </div>
     </section>
   );
--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -8,7 +8,7 @@ interface Link {
 }
 
 interface Project {
-  imgSrc: string;
+  imgSrc: string | null;
   imgAlt: string;
   title: string;
   description: string;
@@ -17,14 +17,44 @@ interface Project {
 
 export const projectsData: Project[] = [
   {
-    imgSrc: "./assets/project-3.png",
-    imgAlt: "Project 3",
-    title: "Explainable AI Research",
-    description: "Investigating how human judgement grounds vary with context towards trustworthy XAI.",
+    imgSrc: "http://www.disaster-medutainment.jp/wp/wp-content/themes/theme_pc/images/learn_disaster_medicine/title.jpg",
+    imgAlt: "災害医療タッチ",
+    title: "災害医療タッチ",
+    description: "いつでもどこでも災害医療を学ぶためのアプリ",
     links: [
-      { label: "GitHub", url: "https://github.com/mei28/explainable-ai-research" },
-      { label: "Paper", url: "https://www.jstage.jst.go.jp/article/pjsai/JSAI2024/0/JSAI2024_2T5OS5b03/_article/-char/ja/" }
+      { label: "HP", url: "http://www.disaster-medutainment.jp/learn_the_disaster_medical/" },
+      { label: "iOS", url: "https://apps.apple.com/jp/app/%E7%81%BD%E5%AE%B3%E5%8C%BB%E7%99%82%E3%82%BF%E3%83%83%E3%83%81/id1139091258" },
+      { label: "Android", url: "https://play.google.com/store/apps/details?id=go.aist.DMTouch" }
     ]
-  }
+  },
+  {
+    imgSrc: "http://www.disaster-medutainment.jp/wp/wp-content/themes/theme_pc/images/textbook_to_junior/title.jpg",
+    imgAlt: "災害医療クエスト",
+    title: "災害医療クエスト",
+    description: "ゲーム感覚でこどもにも災害医療教育のためのアプリ",
+    links: [
+      { label: "HP", url: "http://www.disaster-medutainment.jp/teaching_materials_for_junior/" },
+      { label: "iOS", url: "" },
+      { label: "Android", url: "https://play.google.com/store/apps/details?id=go.aist.DMQuest" },
+    ]
+  },
+  {
+    imgSrc: "http://gesture-interface.jp/wp/wp-content/uploads/2018/09/%E9%87%8D%E5%BA%A6%E9%81%8B%E5%8B%95%E6%A9%9F%E8%83%BD%E9%9A%9C%E5%AE%B3%E8%80%85%E6%94%AF%E6%8F%B4_top-2.png",
+    imgAlt: "ジェスチャーミュージック",
+    title: "ジェスチャーミュージック",
+    description: "ジェスチャーインターフェースのための音楽演奏ゲーム",
+    links: [
+      { label: "HP", url: "http://gesture-interface.jp/download/gesture_music/" },
+    ]
+  },
+  {
+    imgSrc: "https://raw.githubusercontent.com/Seeeeee-D/Hobeeeeee/develop/static/title.png",
+    imgAlt: "Hobeeeeee!!",
+    title: "Hobeeeeee!!",
+    description: "新しい趣味を見つけたい・広めたいという人の願望を叶えるWeb サービス",
+    links: [
+      { label: "HP", url: "https://hobeeeeee.netlify.app/" },
+    ]
+  },
 ];
 
